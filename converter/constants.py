1c2f2568527848de1d33200eb108198cab017ac1
renierkramerhdsr <renier.kramer@hdsr.nl>
2022-10-14 14:04:13 +0200

    run new ingers output

--- converter/constants.py
+++ converter/constants.py
@@ -11,17 +11,21 @@ O_drive = Path("O:")
 BASE_DIR = Path(__file__).parent.parent
 CONVERTER_DIR = BASE_DIR / "converter"
 DATA_DIR = CONVERTER_DIR / "data"
-DATA_INPUT_DIR = DATA_DIR / "input"
 DATA_OUTPUT_DIR = DATA_DIR / "output"
 DATA_EXAMPLE_DIR = DATA_DIR / "example"
 LOG_DIR = DATA_OUTPUT_DIR / "log_rotating"
 LOG_FILE_PATH = LOG_DIR / "main.log"
 
+# PEILMARGE_GIS_EXPORT_DIR = (
+#     O_drive
+#     / "Planvorming\Gebiedsgerichte_plannen\Peilbesluiten\Algemeen\Peilbesluitevaluatie\Product script Inger - peilen, marges en datum"  # noqa
+# )
+# PEILMARGE_GIS_EXPORT_FILE_PATH = PEILMARGE_GIS_EXPORT_DIR / "Peilbesluitpeilen2019_1107.csv"
+
 PEILMARGE_GIS_EXPORT_DIR = (
-    O_drive
-    / "Planvorming\Gebiedsgerichte_plannen\Peilbesluiten\Algemeen\Peilbesluitevaluatie\Product script Inger - peilen, marges en datum"  # noqa
-)
-PEILMARGE_GIS_EXPORT_FILE_PATH = PEILMARGE_GIS_EXPORT_DIR / "Peilbesluitpeilen2019_1107.csv"
+    O_drive / "Planvorming\GIS\Peilbesluiten\Aanpak Actuele Peilbesluiten\Output FME-script"  # noqa
+)  # noqa
+PEILMARGE_GIS_EXPORT_FILE_PATH = PEILMARGE_GIS_EXPORT_DIR / "Koppeling_AAP.csv"
 
 CREATE_CSV_WITH_ERRORS = True
 CREATE_XML = True
@@ -41,6 +45,8 @@ TAB = "    "
 
 
 def get_last_gis_export_peilmarges_csv() -> Path:
+    if PEILMARGE_GIS_EXPORT_FILE_PATH:
+        return PEILMARGE_GIS_EXPORT_FILE_PATH
     list_of_files = glob.glob(pathname=(PEILMARGE_GIS_EXPORT_DIR / "*.csv").as_posix())  # noqa
     assert list_of_files, f"could not find any gis export. We expected at =>1 .csv files in {PEILMARGE_GIS_EXPORT_DIR}"
     latest_file = max(list_of_files, key=os.path.getctime)
@@ -52,7 +58,6 @@ def get_last_gis_export_peilmarges_csv() -> Path:
 def check_constants():
     assert CONVERTER_DIR.is_dir()
     assert DATA_DIR.is_dir()
-    assert DATA_INPUT_DIR.is_dir()
     assert DATA_OUTPUT_DIR.is_dir()
     assert LOG_DIR.is_dir()
 
